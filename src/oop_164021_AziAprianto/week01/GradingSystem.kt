8ad37222155c329ad81ec410ced4395a5e91c99a
Azi <aji.aprinto@gmail.com>
2026-08-29 23:28:20 +0700

    week01: add calculateStatus function

--- src/oop_164021_AziAprianto/week01/GradingSystem.kt
+++ src/oop_164021_AziAprianto/week01/GradingSystem.kt
@@ -12,4 +12,7 @@ fun main() {
         else -> "D"
     }
     println("Grade kamu: $grade")
+    println("Status: ${calculatestatus(score)}")
 }
+
+fun calculatestatus(score: Int) = if (score > 75) "Lulus" else "Tidak Lulus"
