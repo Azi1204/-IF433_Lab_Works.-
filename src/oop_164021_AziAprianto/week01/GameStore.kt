49a1534a752d3d36b2b00229b74dc006770db8d4
Azi <aji.aprinto@gmail.com>
2026-09-03 20:11:02 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_164021_AziAprianto/week01/GameStore.kt
+++ src/oop_164021_AziAprianto/week01/GameStore.kt
@@ -4,7 +4,10 @@ fun main() {
     val gameTitle = "GTA VI"
     val price = 750000
 
-    println("Harga Asli: $price")
+    val finalPrice = calculateDiscount(price)
 }
+
+fun calculateDiscount(price: Int): Int =
+    if (price > 500000) price - (price * 20 / 100) else price - (price * 10 / 100)
 
 
