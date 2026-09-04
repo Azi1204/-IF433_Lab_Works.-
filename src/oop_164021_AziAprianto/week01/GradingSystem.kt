595adcb8ebfdbdc85b6ada2c961c84e811e85a4c
Azi <aji.aprinto@gmail.com>
2026-09-04 20:34:46 +0700

    week01: implement null safety check

--- src/oop_164021_AziAprianto/week01/GradingSystem.kt
+++ src/oop_164021_AziAprianto/week01/GradingSystem.kt
@@ -1,8 +1,24 @@
 package oop_164021_AziAprianto.week01
 
-fun main() {
-    var name: String = "Jhon Thor"
-    var score: Int = 80
+fun calculateStatus(score: Int) = if (score > 75) "Lulus" else "Tidak Lulus"
 
-    println("Nama: " + name + ", Nilai: " + score )
+fun main() {
+    val name = "John Thor"
+    val score = 80
+
+    println("Nama: $name, Nilai: $score")
+
+    val grade = when (score) {
+        in 90..100 -> "A"
+        in 80..89 -> "B"
+        in 70..79 -> "C"
+        else -> "E"
+    }
+    println("Grade kamu: $grade")
+
+    println("Status: ${calculateStatus(score)}")
+
+    val studentId: String? = null
+    val idLength = studentId?.length ?: 0
+    println("Panjang ID: $idLength")
 }
