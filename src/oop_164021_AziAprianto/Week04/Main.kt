90636b8bcde244e1281fda612a4aa83c7f946024
Azi <aji.aprinto@gmail.com>
2026-09-19 11:19:35 +0700

    week04: (task 2) test Employee hierarchy in main

--- src/oop_164021_AziAprianto/Week04/Main.kt
+++ src/oop_164021_AziAprianto/Week04/Main.kt
@@ -17,4 +17,13 @@ fun main(){
     myelectricCar.openTrunk()
     myelectricCar.accelerate()
     myelectricCar.honk()
+
+    println("\n--- Testing Employee hierarchy ---")
+    val manager = Manager(name = "Azi", baseSalary = 1000000000)
+    manager.work()
+    println("Bonus ${manager.name}:Rp ${manager.calculateBonus()}")
+
+    val developer = Developer(name = "Aji", baseSalary = 5000000, progammingLanguage = "Kotlin")
+    developer.work()
+    println("Bonus ${developer.name}:Rp ${developer.calculateBonus()}")
 }
