b80a10c519b93bbb0b8e7992a9a9dd350c51071d
Azi <aji.aprinto@gmail.com>
2026-09-26 10:30:19 +0700

    week05: create Admin class inheriting Pegawai

---
+++ src/oop_164021_AziAprianto/Week05/Admin.kt
@@ -0,0 +1,10 @@
+package oop_164021_AziAprianto.Week05
+
+class Admin(nama: String) : Pegawai(nama) {
+    override fun bekerja() {
+        println("[$nama] sedang duduk di depan komputer melayani administrasi.")
+    }
+    fun doAdminWork(){
+        println("[$nama] sedang merekap data absensi mahasiswa.")
+    }
+}
